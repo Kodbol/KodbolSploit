47e1c506a335ae7d2e1c884958b7c2f14110ba13
kodbol <p.sasha1701@gmail.com>
2025-11-23 13:47:40 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,5 @@
-local games = {
-  12177325772 = print("test")
-}
-
-
-print(12177325772)
+if game.PlaceId == 12177325772 then
+    print("Super League Soccer!")
+elseif game.PlaceId == 123456789 then
+    print("idk game")
+end
