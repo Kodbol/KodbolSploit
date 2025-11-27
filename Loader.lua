0de857ba7cc436138299f44a5f388189be83d512
kodbol <p.sasha1701@gmail.com>
2025-11-27 18:48:55 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-if game.PlaceId == 12177325772 then
+if game.PlaceId == 12177325772 or game.PlaceId == 126195208568849 or game.PlaceId == 127060568647054 then
     print("Super League Soccer!")
 elseif game.PlaceId == 123456789 then
     print("idk game")
